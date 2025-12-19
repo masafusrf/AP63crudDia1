a82049f694fb7e4d5fab0230ab665fd10328ba84
masafu <masafu@alumnatflorida.es>
2025-12-19 10:27:44 +0100

    empiezo v2

--- index.php
+++ index.php
@@ -1,30 +1,36 @@
 <?php
 
     require_once "autoloader.php";
+    session_start();
 
     $gestor= new GestorEntidad();
 
-    for ($i=1; $i<=50; $i++) { 
-        if ($i % 2 == 0) {
-            if ($i % 4 == 0) {
-                $producto= new Electronico($i, "producto$i", $i * 20, "Samsung");
-            } else{
-                $producto= new Electronico($i, "producto$i", $i * 20, "LG");
-            }
-            
-        } else{
+    $accion = $_GET['accion'] ?? null;
 
-            $fecha= date( "d-m-Y", rand(strtotime("2026-01-01"), strtotime("2028-01-01")));
+    if ($accion === 'crear') {
+        
+        $id=uniqid();
+        $nombre= $_POST['nombre'];
+        $precio= $_POST['precio'];
+        $fabricante= $_POST['fabricante'];
+        $caducidad= $_POST['caducidad'];
 
-            $producto= new Alimento($i, "producto$i", $i * 5, $fecha);
-            
-        }
-
+        $producto = new Producto($id, $nombre, $precio, $fabricante, $caducidad);
         $gestor->crearObjeto($producto);
+        header("Location: index.php");
+        exit();
     }
 
-    $gestor->actObj(20, "nuevoProd20", 70);
-    $gestor->actObj(40, "nuevoProd40", 80);
+    $productos = $gestor->leerObj();
+
+    if ($accion === 'actualizar') {
+        $gestor->actObj($_POST['id'], $_POST['nombre'], $_POST['precio']);
+        header("Location: index.php");
+        exit();   
+    }
+
+    // $gestor->actObj(20, "nuevoProd20", 70);
+    // $gestor->actObj(40, "nuevoProd40", 80);
 
     $gestor->elimObj(1);
     $gestor->elimObj(20);
@@ -33,12 +39,32 @@
 
 ?>
 
+<!DOCTYPE html>
+<html>
+<!-- ...-->
+<!-- FORMULARIO CREAR -->
+<h2>Crear Producto</h2>
+
+<form method="POST" action="index.php?accion=crear"> <!-- Aquí está la clave de recoger por GET la acción -->
+
+    <label for="nombre">Nombre</label><br>
+    <input type="text" id="nombre" name="nombre" required><br>
+
+    <label for="precio">Precio</label><br>
+    <input type="number" id="precio" name="precio" required><br>
+
+    <label for="nombre">Fabricante</label><br>
+    <input type="text" id="fabricante" name="fabricante"><br>
+
+    <label for="id">Caducidad</label><br>
+    <input type="date" id="caducidad" name="caducidad"><br>
+
+    <input type="submit" value="Guardar">
+</form>
+
     <!-- LISTADO -->
 <h2>Listado de Productos</h2>
 
-<?php if (empty($productos)): ?>
-    <p>No hay productos aún.</p>
-<?php else: ?>
 
 <table border="1" cellpadding="10">
     <tr>
@@ -47,6 +73,7 @@
         <th>Precio</th>
         <th>Fabricante</th>
         <th>Caducidad</th>
+        <th>Acciones</th>
     </tr>
 
     <?php foreach ($productos as $p): ?>
@@ -56,11 +83,31 @@
         <td><?= $p->getPrecio() ?></td>
         <td><?= ($p instanceof Electronico) ? $p->getFabricante() : 'X' ?></td>
         <td><?= ($p instanceof Alimento) ? $p->getCaducidad() : 'X' ?></td>
+        <td>
+            <!-- Botón Editar -->
+            <form method="POST" action="index.php?accion=actualizar" style="display:inline;"> <!-- Recogemos por GET la acción -->
+                <input type="hidden" name="id" value="<?= $p->getId() ?>">
+                Nombre: <input type="text" name="nombre" value="<?= $p->getNombre() ?>" required>
+                Precio: <input type="number" step= "1" name="precio" value="<?= $p->getPrecio() ?>" required>
+                <?php if ($p instanceof Electronico): ?>
+                    Fabricante:
+                    <input type="text" name="fabricante" value="<?= $p->getFabricante() ?>">
+                <?php endif; ?> 
+                <?php if ($p instanceof Alimento): ?>
+                   Caducidad:
+                    <input type="date" name="caducidad" value="<?= $p->getCaducidad() ?>">
+                <?php endif; ?>
+
+                <button type="submit">Guardar</button>    
+            <!-- recogemos por POST los datos para modificar (podemos poner datos por defecto) -->
+            </form>
+
+        </td>
     </tr>
     <?php endforeach; ?>
 </table>
 
-<?php endif; ?>
+
 
 </body>
 </html>
--- src/GestorEntidad.php
+++ src/GestorEntidad.php
@@ -2,31 +2,36 @@
 
     class GestorEntidad{
 
-        private $productos= [];
-
-
-        public function crearObjeto($nuevoObjeto){
-            $this->productos[]= $nuevoObjeto;
-        }
-
-        public function leerObj(){
-            return $this->productos;
-        }
-
-        public function buscarObjeto($id){
-            foreach ($this->productos as $objeto) {
-                if ($objeto ->getId() == $id) {
-                    return $objeto;
-                }
+        public function __construct() {
+            if (!isset($_SESSION['productos'])) {
+                $_SESSION['productos'] = [];
             }
         }
 
-        public function actObj($id, $nombre, $precio){
-            $objeto = $this->buscarObjeto($id);
+        public function crearObjeto($p) {
+            $_SESSION['productos'][] = $p;
+        }
 
-            if ($objeto !== null) {
-                $objeto->setNombre($nombre);
-                $objeto->setPrecio($precio);
+        public function leerObj(){
+            return $_SESSION['productos'];
+        }
+
+        public function buscarObjeto($id){
+            foreach ($_SESSION['productos'] as $p) {
+                if ($p ->getId() == $id) {
+                    return $p;
+                }
+            }
+
+            return null;
+        }
+
+        public function actObj($id, $nombre, $precio){
+            $p= $this->buscarObjeto($id);
+
+            if ($p !== null) {
+                $p->setNombre($nombre);
+                $p->setPrecio($precio);
                 return true;
             }
 
@@ -35,24 +40,69 @@
 
         public function elimObj($id){
 
-            foreach ($this->productos as $i => $objeto) {
+            foreach ($_SESSION['productos'] as $i => $objeto) {
                 if ($objeto->getId() == $id) {
-                    // unset elimina la posicion i del array
-                    unset($this->productos[$i]);
-                    // array_values reorganiza el array para 
-                    // que los indices sigan siendo 0 1 2 3 y no 
-                    // quede un salto dsps de borrar 1 elmto (0 2 3)
-                    $this->productos = array_values($this->productos);
+                    unset($_SESSION['productos'][$i]);
+                    $_SESSION['productos'] = array_values($_SESSION['productos']);
 
                     return true;
                 }
             }
 
             return false;
-
         }
 
-        
     }
+
+
+        // public function crearObjeto($nuevoObjeto){
+        //     $this->productos[]= $nuevoObjeto;
+        // }
+
+        // public function leerObj(){
+        //     return $this->productos;
+        // }
+
+        // public function buscarObjeto($id){
+        //     foreach ($this->productos as $objeto) {
+        //         if ($objeto ->getId() == $id) {
+        //             return $objeto;
+        //         }
+        //     }
+        // }
+
+        // public function actObj($id, $nombre, $precio){
+        //     $objeto = $this->buscarObjeto($id);
+
+        //     if ($objeto !== null) {
+        //         $objeto->setNombre($nombre);
+        //         $objeto->setPrecio($precio);
+        //         return true;
+        //     }
+
+        //     return false;
+        // }
+
+    //     public function elimObj($id){
+
+    //         foreach ($this->productos as $i => $objeto) {
+    //             if ($objeto->getId() == $id) {
+    //                 // unset elimina la posicion i del array
+    //                 unset($this->productos[$i]);
+    //                 // array_values reorganiza el array para 
+    //                 // que los indices sigan siendo 0 1 2 3 y no 
+    //                 // quede un salto dsps de borrar 1 elmto (0 2 3)
+    //                 $this->productos = array_values($this->productos);
+
+    //                 return true;
+    //             }
+    //         }
+
+    //         return false;
+
+    //     }
+
+        
+    // }
 
 ?>
